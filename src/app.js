eaf90b025b346df786f071013ef269dee53fb7d0
rhammock1 <rhamm1027@gmail.com>
2020-11-30 16:29:22 -0500

    fixed syntax error

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-require('dotenv').config;
+require('dotenv').config();
 
 const app = express();
 
